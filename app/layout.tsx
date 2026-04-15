d421b7f1f7eafb97a4c199643cca6f2241a50881
Enochchukwudi-dev <your-email@example.com>
2026-04-15 16:08:19 +0100

    Done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import FacebookPixel from "@/components/facebook-pixel";
 
 export const metadata: Metadata = {
   title: "Tech Trailblazer Academy - We Tech Different",
@@ -90,6 +91,19 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-5XK2C438');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
         <script
           dangerouslySetInnerHTML={{
             __html: `
@@ -112,57 +126,26 @@ export default function RootLayout({
         />
         <meta name="facebook-domain-verification" content="duxa7kyhhskwb27009uj5ns45kvc9o" />
         <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '2105318860009966');
-              fbq('track', 'PageView');
-
-              // Track page views on route changes for Next.js
-              if (typeof window !== 'undefined' && window.fbq) {
-                const originalPushState = history.pushState;
-                const originalReplaceState = history.replaceState;
-
-                history.pushState = function(...args) {
-                  originalPushState.apply(this, args);
-                  setTimeout(() => window.fbq('track', 'PageView'), 0);
-                };
-
-                history.replaceState = function(...args) {
-                  originalReplaceState.apply(this, args);
-                  setTimeout(() => window.fbq('track', 'PageView'), 0);
-                };
-
-                window.addEventListener('popstate', () => {
-                  setTimeout(() => window.fbq('track', 'PageView'), 0);
-                });
-              }
-            `,
-          }}
-        />
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: 'none' }}
-            src="https://www.facebook.com/tr?id=2105318860009966&ev=PageView&noscript=1"
-          />
-        </noscript>
-        <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
             __html: JSON.stringify(structuredData),
           }}
         />
       </head>
-      <body>{children}</body>
+      <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5XK2C438"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        <FacebookPixel />
+        {children}
+      </body>
     </html>
   );
 }
