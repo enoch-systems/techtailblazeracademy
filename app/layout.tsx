c8534d4eef2034d7159cf70bd7f0699738a5d1f0
Enochchukwudi-dev <your-email@example.com>
2026-04-15 16:37:01 +0100

    Done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import FacebookPixel from "@/components/facebook-pixel";
-import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "Tech Trailblazer Academy - We Tech Different",
@@ -113,6 +111,32 @@ export default function RootLayout({
           }}
         />
         <meta name="facebook-domain-verification" content="duxa7kyhhskwb27009uj5ns45kvc9o" />
+        {/* Meta Pixel Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '808967788955487');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=808967788955487&ev=PageView&noscript=1"
+          />
+        </noscript>
+        {/* End Meta Pixel Code */}
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
@@ -121,9 +145,6 @@ export default function RootLayout({
         />
       </head>
       <body>
-        <Suspense fallback={null}>
-          <FacebookPixel />
-        </Suspense>
         {children}
       </body>
     </html>
